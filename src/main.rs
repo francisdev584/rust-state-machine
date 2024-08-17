97ce75e64d031a3591ee4b32fb3eb535efb2ed32
Francinildo Rodrigues <francis584@gmail.com>
2024-08-17 20:03:53 -0300

    simulating blocks in our blockchain runtime

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,30 @@ impl Runtime {
 	}
 }
 fn main() {
-	let runtime = Runtime::new();
-	println!("hello, Francis!");
+	let mut runtime = Runtime::new();
+
+	let alice = "alice".to_string();
+	let bob = "bob".to_string();
+	let charlie = "charlie".to_string();
+
+	runtime.balances.set_balance(&alice.clone(), 100);
+
+	runtime.system.inc_block_number();
+
+	// assert_eq!(runtime.system.block_number(), 1);
+
+	// first transaction
+	runtime.system.inc_nonce(&alice);
+
+	let _ = runtime
+		.balances
+		.transfer(alice.clone(), bob.clone(), 30)
+		.map_err(|e| println!("Error:{:?}", e));
+
+		runtime.system.inc_nonce(&alice);
+
+		let _ = runtime
+			.balances
+			.transfer(alice.clone(), charlie.clone(), 20)
+			.map_err(|e| println!("Error:{:?}", e));
 }
