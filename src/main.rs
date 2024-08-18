04eea66c29d0652f249e6bfa298767a5ea362a8e
Francinildo Rodrigues <francis584@gmail.com>
2024-08-18 02:37:50 -0300

    dispatching calls

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
+use support::Dispatch;
+
 mod balances;
-mod system;
 mod support;
+mod system;
 
 mod types {
-    use crate::support;
+	use crate::support;
 
 	pub type AccountId = String;
 	pub type Balance = u128;
 	pub type BlockNumber = u32;
 	pub type Nonce = u32;
-	pub type Extrinsic = support::Extrinsic<AccountId,crate::RuntimeCall>;
+	pub type Extrinsic = support::Extrinsic<AccountId, crate::RuntimeCall>;
 	pub type Header = support::Header<BlockNumber>;
 	pub type Block = support::Block<Header, Extrinsic>;
 }
 
-enum RuntimeCall {
-	
+pub enum RuntimeCall {
+	BalancesTransfer { to: types::AccountId, amount: types::Balance },
 }
 
 impl system::Config for Runtime {
@@ -39,6 +41,50 @@ impl Runtime {
 	// Create a new instance of the main Runtime, by creating a new instance of each pallet.
 	fn new() -> Self {
 		Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
+	}
+
+	fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
+		self.system.inc_block_number();
+
+		let block_number = self.system.block_number();
+
+		if block_number != block.header.block_number {
+			return Err("Block number mismatch");
+		}
+
+		for (i, support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
+			self.system.inc_nonce(&caller);
+			let _ = self.dispatch(caller, call).map_err(|e| {
+				eprintln!(
+					"Extrinsic Error\n\tBlock Number: {}\n\tExtrinsic Number: {}\n\tError: {}",
+					block.header.block_number, i, e
+				)
+			});
+		}
+
+		Ok(())
+	}
+}
+
+impl crate::support::Dispatch for Runtime {
+	type Caller = <Runtime as system::Config>::AccountId;
+	type Call = RuntimeCall;
+	// Dispatch a call on behalf of a caller. Increments the caller's nonce.
+	//
+	// Dispatch allows us to identify which underlying module call we want to execute.
+	// Note that we extract the `caller` from the extrinsic, and use that information
+	// to determine who we are executing the call on behalf of.
+	fn dispatch(
+		&mut self,
+		caller: Self::Caller,
+		runtime_call: Self::Call,
+	) -> support::DispatchResult {
+		match runtime_call {
+			RuntimeCall::BalancesTransfer { to, amount } => {
+				self.balances.transfer(caller, to, amount)?;
+			},
+		}
+		Ok(())
 	}
 }
 fn main() {
