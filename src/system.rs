cb5c5d77b4298e59ead9fc7458a35646c16ef50b
Francinildo Rodrigues <francis584@gmail.com>
2024-08-18 00:37:16 -0300

    adding generics to System Pallet

--- src/system.rs
+++ src/system.rs
@@ -1,17 +1,21 @@
 use std::collections::BTreeMap;
 
-type AccountId = String;
-type BlockNumber = u32;
-type Nonce = u32;
+use num::traits::{CheckedAdd, One, Zero};
+
 #[derive(Debug)]
-pub struct Pallet {
+pub struct Pallet<AccountId, BlockNumber, Nonce> {
 	block_number: BlockNumber,
 	nonce: BTreeMap<AccountId, Nonce>,
 }
 
-impl Pallet {
+impl<AccountId, BlockNumber, Nonce> Pallet<AccountId, BlockNumber, Nonce>
+where
+	AccountId: Ord + Clone,
+	BlockNumber: Zero + One + CheckedAdd,
+	Nonce: Zero + One + Copy + CheckedAdd,
+{
 	pub fn new() -> Self {
-		Self { block_number: 0, nonce: BTreeMap::new() }
+		Self { block_number: BlockNumber::zero(), nonce: BTreeMap::new() }
 	}
 
 	pub fn block_number(self) -> BlockNumber {
@@ -20,17 +24,17 @@ impl Pallet {
 
 	pub fn inc_block_number(&mut self) {
 		// crashes if overflow for purpose
-		self.block_number = self.block_number.checked_add(1).unwrap();
+		self.block_number = self.block_number.checked_add(&BlockNumber::one()).unwrap();
 	}
 
 	pub fn inc_nonce(&mut self, who: &AccountId) {
-		let nonce = self.nonce.get(who).unwrap_or(&0);
-		let new_nonce = nonce.checked_add(1).unwrap();
+		let nonce = *self.nonce.get(who).unwrap_or(&Nonce::zero());
+		let new_nonce = nonce.checked_add(&Nonce::one()).unwrap();
 		self.nonce.insert(who.clone(), new_nonce);
 	}
 
 	pub fn get_nonce(&self, who: &AccountId) -> Nonce {
-		*self.nonce.get(who).unwrap_or(&0)
+		*self.nonce.get(who).unwrap_or(&Nonce::zero())
 	}
 }
 
@@ -38,14 +42,14 @@ impl Pallet {
 mod test {
 	#[test]
 	fn init_system() {
-		let system = super::Pallet::new();
+		let system = super::Pallet::<String, u32, u32>::new();
 
 		assert_eq!(system.block_number(), 0);
 	}
 
 	#[test]
 	fn inc_block_number() {
-		let mut system = super::Pallet::new();
+		let mut system = super::Pallet::<String, u32, u32>::new();
 		system.inc_block_number();
 
 		assert_eq!(system.block_number(), 1);
@@ -54,7 +58,7 @@ mod test {
 	#[test]
 	fn inc_nonce() {
 		let alice = String::from("alice");
-		let mut system = super::Pallet::new();
+		let mut system = super::Pallet::<String, u32, u32>::new();
 
 		system.inc_nonce(&alice);
 
